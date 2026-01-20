d9d0945990db3eb6e0901af67e0e58090da798c5
= <rajmandersinghmatharu@gmail.com>
2026-01-20 14:43:25 +0530

    changes

--- dbConnect.js
+++ dbConnect.js
@@ -4,7 +4,7 @@ const db = async () => {
   try {
     //await mongoose.connect("mongodb://localhost:27017/testdb");
     await mongoose.connect(
-      "mongodb+srv://rajmanderdev_db_user:f82lMIHkh6Gzp0z7@rajcluster.k52h8mf.mongodb.net/?appName=rajcluster",
+      "mongodb+srv://rajmanderdev_db_user:f82lMIHkh6Gzp0z7@rajcluster.k52h8mf.mongodb.net/enery_drinks?appName=rajcluster",
     );
 
     console.log("connected");
